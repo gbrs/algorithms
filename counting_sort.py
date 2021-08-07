ed25a241e0eeda462f72a3bf853758ec1811c008
gbrs <gbrs@mail.ru>
2021-08-07 11:51:57 +0300

    solved counting sort with collections.Counter

--- counting_sort.py
+++ counting_sort.py
@@ -1,16 +1,24 @@
+'''
+Проходим по списку/строке и подсчитаем количество вхождений каждого элемента.
+А затем выводим элементы в "алфавитном" порядке столько раз, сколько мы насчитали.
+Целесообразна лишь тогда, когда сортируемые сортируемых значений мало
+по сравнению с сортируемым множеством.
+'''
+
 from random import choice
+from collections import Counter
 
+# случайная генерация текста из заданного набора символов
 text = ''
-text_length = 1000
-random_string = 'jhjkYhxKl kGxh3470->?/<Q'
-alphabet = tuple(
-                sorted(
-                    list(
-                        set(random_string)
-                    )
-                )
-            )
-
+text_length = 80
+used_symbols = 'jhjkYhxKl kGxh3470->?/<Q'
+alphabet = tuple(set(used_symbols))
 for _ in range(text_length):
     text = ''.join((text, choice(alphabet)))
-print(text)
+
+count_dictionary = dict(Counter(text))
+
+for symbol in sorted(count_dictionary):
+    for _ in range(count_dictionary[symbol]):
+        print(symbol, end='')
+print()
