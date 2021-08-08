a939756eddde45de12b15cf1ef0def4fe4fec919
gbrs <gbrs@mail.ru>
2021-08-08 11:45:37 +0300

    wrote counting sort

--- counting_sort.py
+++ counting_sort.py
@@ -5,20 +5,27 @@
 по сравнению с сортируемым множеством.
 '''
 
+
 from random import choice
-from collections import Counter
+# from collections import Counter
 
 # случайная генерация текста из заданного набора символов
+TEXT_LENGTH = 80
 text = ''
-text_length = 80
 used_symbols = 'jhjkYhxKl kGxh3470->?/<Q'
 alphabet = tuple(set(used_symbols))
-for _ in range(text_length):
+for _ in range(TEXT_LENGTH):
     text = ''.join((text, choice(alphabet)))
 
-count_dictionary = dict(Counter(text))
+# подсчитываем количество вхождений для каждого символа, используя словарик
+# count_dictionary = dict(Counter(text))  # решение с помощью collections.Counter
+count_dictionary = dict()
+for symbol in text:
+    count_dictionary[symbol] = count_dictionary.setdefault(symbol, 0) + 1
 
+# создание сортированного списка
+sorted_list = []
 for symbol in sorted(count_dictionary):
     for _ in range(count_dictionary[symbol]):
-        print(symbol, end='')
-print()
+        sorted_list.append(symbol)
+print(*sorted_list, sep='')
