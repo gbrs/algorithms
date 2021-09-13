259c4e5065fd7b8e08fed613125aa9a0028e7430
gbrs <gbrs@mail.ru>
2021-09-13 10:55:48 +0300

    finished insertion sort

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,5 +1,3 @@
-# TODO недоделан
-
 '''
 Сортировка вставками (англ. Insertion sort) — алгоритм сортировки,
 в котором элементы просматриваются последовательно
@@ -10,29 +8,32 @@
 from random import randint
 
 # генерируем случайный список
-list_size = 5
-# lst = [randint(-10, 10) for i in range(list_size)]
-lst = [7, 6, 9, 8, 1]
+list_size = 1000
+lst = [randint(-100, 100) for i in range(list_size)]
 spam = None
-print(0, 0, 0, *lst)
+print(*lst)
 
+# первый элемент - уже отсортированная часть массива,
+# перебираем все остальные.
 for i in range(1, list_size):
-    for j in range(i - 1, -1, -1):  # обратное движение обеспечит устойчивость сортировки
+    # пробегаем отсортированную часть массива (до i),
+    # подыскивая подходящее место для вставки.
+    # Обратное движение обеспечит устойчивость сортировки
+    for j in range(i - 1, -1, -1):
         if lst[j] <= lst[i]:
             spam = lst[i]
             # сдвиг вправо
-            for k in range(i, j + 1, -1):
+            for k in range(i, j, -1):
                 lst[k] = lst[k - 1]
+            # вставка текущего элемента в нужное место
             lst[j + 1] = spam
             break
-        else:
-            spam = lst[i]
-            # сдвиг вправо
-            for k in range(i, 0, -1):
-                lst[k] = lst[k - 1]
-            lst[0] = spam
+    # если не нашли подходящего места - значит текущий элемент надо вставить в начало
+    else:
+        spam = lst[i]
+        for k in range(i, 0, -1):
+            lst[k] = lst[k - 1]
+        lst[0] = spam
+    # print(*lst)
 
-    print(i, j, spam, *lst)
-
-
-
+print(*lst)
