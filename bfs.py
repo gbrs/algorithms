7c08f25beb10085e2eab6679187f75d23ddfdacc
gbrs <gbrs@mail.ru>
2022-04-03 12:22:26 +0300

    BFS

---
+++ bfs.py
@@ -0,0 +1,62 @@
+"""
+обход в ширину BFS графа
+"""
+
+from collections import deque
+
+# описываем граф, используя списки смежности
+GRAPH = {
+    1: [2, 3, 4],
+    2: [1, 5, 6],
+    3: [1],
+    4: [1, 7, 8],
+    5: [2, 9, 10],
+    6: [2],
+    7: [4, 11, 12],
+    8: [4],
+    9: [5],
+    10: [5],
+    11: [7],
+    12: [7]
+}
+# https://lisiynos.github.io/s1/graph_alg/breadth-first-tree.png
+
+'''GRAPH = {
+    1: [2, 8],
+    2: [1, 3, 8],
+    3: [2, 4, 8],
+    4: [3, 7, 9],
+    5: [6, 7],
+    6: [5],
+    7: [4, 5, 8],
+    8: [1, 2, 3, 7],
+    9: [4],
+}
+# https://lisiynos.github.io/s1/graph_alg/primer.gif
+'''
+
+# создаем список для посещенных вершин,
+# очередь для будущего посещения
+# и список последовательности обхода
+visited_nodes = []
+queue = deque()
+traversal_sequence = []
+
+START_NODE = 1
+
+visited_nodes.append(START_NODE)
+queue.append(START_NODE)
+
+while queue:
+    # пока очередь непустая
+    # обрабатываем левую вершину очереди (explored_node):
+    # печатаем ее, попаем и добавляем всех ее соседок в очередь
+    explored_node = queue.popleft()
+    traversal_sequence.append(explored_node)
+
+    for neighbor in GRAPH[explored_node]:
+        if neighbor not in visited_nodes:
+            visited_nodes.append(neighbor)
+            queue.append(neighbor)
+
+print(*traversal_sequence, sep=' -> ')
--- dfs.py
+++ dfs.py
@@ -1,29 +1,45 @@
 """
-рекурсивный DFS. Прямой - NLR.
+рекурсивный DFS. Прямой обход: NLR.
 """
 
 
-def dfs(vertica):
+def dfs(node):
     """
-    рекурсивно обходим граф
-    :param vertica: вершина, которую исследуем
+    рекурсивно обходим граф: NLR
+    :param node: вершина, которую исследуем
     :return: None
     внешние эффекты: изменяем список visited_vertices
     """
 
     # если вершина уже посещена, выходим в предыдущий этап стека вызовов (шаг назад по дереву)
-    if vertica in visited_vertices:
+    if node in visited_nodes:
         return
 
-    visited_vertices.append(vertica)  # записываем вершину в посещенные
+    visited_nodes.append(node)  # записываем вершину в посещенные
 
-    for neighbor in graph[vertica]:  # обходим все смежные вершины
-        if neighbor not in visited_vertices:
+    for neighbor in GRATH[node]:  # обходим все смежные вершины
+        if neighbor not in visited_nodes:
             dfs(neighbor)  # рекурсивно проваливаясь в еще не посещенные
 
 
 # описываем граф, используя списки смежности
-graph = {
+GRATH = {
+    1: [2, 3, 4],
+    2: [1, 5, 6],
+    3: [1],
+    4: [1, 7, 8],
+    5: [2, 9, 10],
+    6: [2],
+    7: [4, 11, 12],
+    8: [4],
+    9: [5],
+    10: [5],
+    11: [7],
+    12: [7]
+}
+# https://lisiynos.github.io/s1/graph_alg/breadth-first-tree.png
+
+'''graph = {
     1: [2, 8],
     2: [1, 3, 8],
     3: [2, 4, 8],
@@ -35,12 +51,13 @@ graph = {
     9: [4],
 }
 # https://lisiynos.github.io/s1/graph_alg/primer.gif
+'''
 
 # создаем список для посещенных вершин.
 # А стек у нас создается за счет стека рекурсивных вызовов?
-visited_vertices = []
+visited_nodes = []
 
-start_vertica = 1
-dfs(start_vertica)
+START_NODE = 1
+dfs(START_NODE)
 
-print(visited_vertices)
+print(*visited_nodes, sep=' -> ')
